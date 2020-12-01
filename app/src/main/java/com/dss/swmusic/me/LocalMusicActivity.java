aa0e3c8b025407ae20178c94f0882e2f4b746db0
nange77456 <ssdeng0@qq.com>
2020-12-01 10:42:01 +0800

    first commit

---
+++ app/src/main/java/com/dss/swmusic/me/LocalMusicActivity.java
@@ -0,0 +1,25 @@
+package com.dss.swmusic.me;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dss.swmusic.R;
+import com.dss.swmusic.databinding.ActivityLocalMusicBinding;
+
+public class LocalMusicActivity extends AppCompatActivity {
+
+    private ActivityLocalMusicBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_local_music);
+
+        binding = ActivityLocalMusicBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+//        binding.localMusicRecycler
+
+    }
+}
